6b0114e6b111429bf7f8c30be8b9a6d0f8707b1d
PRAVEEN KUMAR JAYSWAL <praveenjayswal625@gmail.com>
2025-11-17 13:29:31 +0530

    java

---
+++ LECT 3/Main.java
@@ -0,0 +1,57 @@
+// class main {
+//     public static void main(String[] args) {
+//         String str = "Hello, praveen";
+//          system.out.println(str.length());
+//         System.out.println(str.charAt(4));
+//         System.out.println(str.toUpperCase());
+//         System.out.println(str.toLowerCase());
+//         System.out.println(str.indexOf('e'));
+//         System.out.println(str.substring(2));
+//         System.out.println(str.substring(2,5));
+//         system.out.println(str.length());
+//         system.out.println(str.trim());
+//     }
+// }
+
+
+// check two strings are equal or not
+// class main {
+//     public static void main(String[] args) {
+//         String str=new String("hello");
+//         String str1=new String("hello");
+//         System.out.println(str==str1);
+//         System.out.println(str.equals(str1));
+//     }
+// }
+
+// check palindrome
+// class main {
+//     public static void main(String[] args) {
+        
+//     }
+//         String a="abc";
+//         String b="";
+//         for(int i=a.length()-1;i>=0;i--){
+//             b+=a.charAt(i);
+//         }
+//         if(a.equals(b)){
+//             System.out.println("ji hai");
+//             }else{
+//                 System.out.println("nahi hai");
+//             }
+//     }
+// }
+// strings are immutable in java
+// once a string is created, it cannot be changed
+// any modification to a string will create a new string
+// Check vowels and consonants in a string
+// class main {
+//     public static void main(String[] args) {
+//         String str = "Hello";
+//         String a[] = str,split("");
+//         System.out.println(a[l]);
+//         for(String str1 : a)
+//         {System.out.println(str1);
+//             }
+//         }
+//     }
